b48bbb2527094faac87bc7575a65481fd5c4af0e
FakhriMadiouni <fakhri.madiouni@gmail.com>
2026-05-01 20:11:24 +0200

    Delete backend-sprint1 directory

--- backend-sprint1/src/main/java/com/library/livres/entities/LivreProjection.java
+++
@@ -1,9 +0,0 @@
-package com.library.livres.entities;
-
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "titreLiv", types = { Livre.class })
-public interface LivreProjection {
-    public String getTitreLivre();
-    public Double getPrixLivre();
-}
--- backend-sprint1/src/main/java/com/library/livres/repos/AuteurRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.livres.repos;
-
-import com.library.livres.entities.Auteur;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "auteurs-rest")
-public interface AuteurRepository extends JpaRepository<Auteur, Long> {
-}
